a6c4bf7a6d58baaf64213a07a759d7de935bf1e2
Adam Wiktor <adam.wiktor@vimncontractor.com>
2019-03-09 13:16:23 +0100

    Add saving and restoring models

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -9,7 +9,8 @@ file_name = "run_auto_increment"
 
 
 class NeuralNetwork(object):
-    def __init__(self, input_dim, sequence, output_dim, learning_rate=0.1, scope="main", gather_stats=True):
+    def __init__(self, input_dim, sequence, output_dim, learning_rate=0.1, scope="main", gather_stats=True,
+                 restore_model=False, save_model=False, restore_model_path=None, save_model_path=None):
         self.scope = scope
         self.sequence = sequence
         self.learning_rate = tf.constant(learning_rate)
@@ -19,6 +20,7 @@ class NeuralNetwork(object):
         self.gather_stats = gather_stats
         self.handle = tf.placeholder(tf.string, shape=[])
         self.__init_run_number()
+        self.__init_model_saving(restore_model, save_model, restore_model_path, save_model_path)
         self.build()
         self.merged_summary = tf.summary.merge_all()
 
@@ -31,6 +33,18 @@ class NeuralNetwork(object):
         self.run_number += 1
         with open(file_name, 'w') as file:
             file.write(str(self.run_number))
+
+    def __init_model_saving(self, restore_model, save_model, restore_model_path, save_model_path):
+        self.restore_model = restore_model
+        self.save_model = save_model
+        if restore_model_path is not None:
+            self.restore_model_path = restore_model_path
+        else:
+            self.restore_model_path = f"./saved_model_{self.scope}_{self.run_number - 1}/model.ckpt"
+        if save_model_path is not None:
+            self.save_model_path = save_model_path
+        else:
+            self.save_model_path = f"./saved_model_{self.scope}_{self.run_number}/model.ckpt"
 
     def build(self):
         raise NotImplementedError("This method should be implemented in subclass")
@@ -50,24 +64,24 @@ class NeuralNetwork(object):
     def __train_all_epochs(self, training_it, validation_it, batch, batch_size, epochs, eval_period, stat_period):
         writer, val_writer = self.__init_writers()
         training_handle, validation_handle = self.__init_handlers(training_it, validation_it)
-        self.sess.run(tf.global_variables_initializer())
+        self.__init_global_variables()
         self.counter = 0
         self.epoch = 0
-        print(f"Start training for epochs={epochs} with batch_size={batch_size}")
+        print(f"start training for epochs={epochs} with batch_size={batch_size}")
         for _ in range(epochs):
+            res = self.__validate(batch, validation_it, validation_handle)
+            print(f"start epoch {self.epoch}, accuracy: {res}%")
             self.__train_single_epoch(training_it, validation_it, training_handle, validation_handle, batch, writer,
                                       val_writer, eval_period, stat_period)
-            res = self.__validate(batch, validation_it, validation_handle)
-            print("epoch {}:  {}%".format(self.epoch, res))
 
         res = self.__validate(batch, validation_it, validation_handle)
-        print("total {}%".format(res))
+        print(f"total accuracy: {res}%")
         self.__close_writers(writer, val_writer)
-        # TODO save model
+        self.__maybe_save_model()
 
     def __init_writers(self):
-        writer = tf.summary.FileWriter("./demo/{}_{}".format(self.scope, self.run_number), self.sess.graph)
-        val_writer = tf.summary.FileWriter("./demo/val_{}_{}".format(self.scope, self.run_number), self.sess.graph)
+        writer = tf.summary.FileWriter(f"./demo/{self.scope}_{self.run_number}", self.sess.graph)
+        val_writer = tf.summary.FileWriter(f"./demo/val_{self.scope}_{self.run_number}", self.sess.graph)
         return writer, val_writer
 
     def __close_writers(self, writer, val_writer):
@@ -78,6 +92,21 @@ class NeuralNetwork(object):
         training_handle = self.sess.run(training_it.string_handle())
         validation_handle = self.sess.run(validation_it.string_handle())
         return training_handle, validation_handle
+
+    def __init_global_variables(self):
+        if self.restore_model:
+            saver = tf.train.Saver()
+            saver.restore(self.sess, self.restore_model_path)
+            print(f"model restored from path {self.restore_model_path}")
+        else:
+            self.sess.run(tf.global_variables_initializer())
+            print("fresh model")
+
+    def __maybe_save_model(self):
+        if self.save_model:
+            saver = tf.train.Saver()
+            saver.save(self.sess, self.save_model_path)
+            print(f"model saved in path {self.save_model_path}")
 
     def __train_single_epoch(self, training_it, validation_it, training_handle, validation_handle, batch, writer,
                              val_writer, eval_period, stat_period):
@@ -91,7 +120,7 @@ class NeuralNetwork(object):
                     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
                     run_metadata = tf.RunMetadata()
                     summary, _ = self.sess.run([self.merged_summary, self.step], feed_dict, run_options, run_metadata)
-                    writer.add_run_metadata(run_metadata, 'step_%d' % self.counter)
+                    writer.add_run_metadata(run_metadata, f"step_{self.counter}")
                 else:
                     summary, _ = self.sess.run([self.merged_summary, self.step], feed_dict)
 
@@ -99,7 +128,7 @@ class NeuralNetwork(object):
 
                 if self.counter % eval_period is 0:
                     res = self.__validate(batch, validation_it, validation_handle, val_writer)
-                    print("iter: {}, acc: {}%".format(self.counter, res))
+                    print(f"iteration: {self.counter}, accuracy: {res}%")
 
                 self.counter += 1
             except tf.errors.OutOfRangeError:
@@ -131,21 +160,23 @@ class NeuralNetwork(object):
         return res
 
     def infer(self, x):
-        # TODO restore model
+        saver = tf.train.Saver()
         with tf.Session() as sess:
-            res = sess.run(self.result, {self.features: x})
+            saver.restore(sess, self.restore_model_path)
+            res = sess.run(self.result, feed_dict={self.features: x})
         return res
 
     def test(self, data_set, batch_size=10):
-        # TODO restore model
         next_batch = data_set.batch(batch_size).make_one_shot_iterator().get_next()
         total_res = 0
         counter = 0
+        saver = tf.train.Saver()
         with tf.Session() as sess:
+            saver.restore(sess, self.restore_model_path)
             while True:
                 try:
                     batch_xs, batch_ys = sess.run(next_batch)
-                    res = sess.run(self.acct_res, {self.features: batch_xs, self.labels: batch_ys})
+                    res = sess.run(self.acct_res, feed_dict={self.features: batch_xs, self.labels: batch_ys})
                     total_res += res
                     counter += batch_size
                 except tf.errors.OutOfRangeError:
