d1a86bc81b3215f33ed855eeb05502b684455369
Tien Chu <chutien@protonmail.com>
2019-03-10 16:13:51 +0100

    Layers, Block, WeightLayer, propagate functions, refreshed FA and DFA, BackwardPropagation intermediate class (#17)
    * Layers, Block, WeightLayer, propagate functions, refreshed FA and DFA, BackwardPropagation intermediate class

---
+++ backward_propagation.py
@@ -0,0 +1,22 @@
+from layer import *
+from loader import *
+from neuralnetwork import NeuralNetwork
+
+
+class BackwardPropagation(NeuralNetwork):
+    def build_forward(self):
+        a = self.features
+        for block in self.sequence:
+            a = block.head.build_forward(a, remember_input=True)
+            for layer in block.tail:
+                a = layer.build_forward(a, remember_input=True)
+        return a
+
+    def build_backward(self, output_vec):
+        error = tf.subtract(output_vec, self.labels)
+        self.step = []
+        for block in reversed(self.sequence):
+            for layer in reversed(list(block)):
+                error = layer.build_backward(error)
+                if layer.trainable:
+                    self.step.append(layer.step)
--- feedback_alignment.py
+++ feedback_alignment.py
@@ -1,25 +1,21 @@
-import tensorflow as tf
-from neuralnetwork import NeuralNetwork
-from utils import *
+from layer import *
+from loader import *
+from backward_propagation import BackwardPropagation
+from propagate import feedback_alignment
 
-
-class FANeuralNetwork(NeuralNetwork):
-    def backpropagation_fully_conneted(self, input_error, z, a, scope):
-        with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
-            weights = tf.get_variable("weights")
-            random_weights = tf.get_variable("random_weights", shape=tf.transpose(weights).get_shape().as_list(),
-                initializer=tf.random_normal_initializer())
-            biases = tf.get_variable("biases")
-            error = tf.multiply(input_error, sigmoid_prime(z))
-            delta_biases = error
-            delta_weights = tf.matmul(tf.transpose(a), error)
-            output_error = tf.matmul(error, random_weights)
-            weights = tf.assign(weights, tf.subtract(weights, tf.multiply(self.learning_rate, delta_weights)))
-            biases = tf.assign(biases, tf.subtract(biases, tf.multiply(self.learning_rate, tf.reduce_mean(delta_biases,
-                axis=[0]))))
-            return output_error, weights, biases
+class FeedbackAlignment(BackwardPropagation):
+    def set_propagate_functions(self):
+        for block in self.sequence:
+            block.head.propagate_func = feedback_alignment
 
 
 if __name__ == '__main__':
-    FA = FANeuralNetwork([("-", 784), ("f", 50), ("a", 50), ("f", 10), ("a", 10)], scope="FA")
-    FA.train()
+    training, test = load_mnist()
+    NN = FeedbackAlignment(784,
+                           [Block([FullyConnected(50), BatchNormalization(), Sigmoid()]),
+                            Block([FullyConnected(30), BatchNormalization(), Sigmoid()]),
+                            Block([FullyConnected(10), Sigmoid()])],
+                           10,
+                           0.1,
+                           'FA')
+    NN.train(training, test)
