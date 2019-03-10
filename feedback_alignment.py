1ffd2860bf0f34cb745606c74c870c1487a4939f
Adam Wiktor <adam.wiktor@vimncontractor.com>
2019-03-10 17:53:05 +0100

    Dont save meta graph after training - save only global variables

--- feedback_alignment.py
+++ feedback_alignment.py
@@ -3,6 +3,7 @@ from loader import *
 from backward_propagation import BackwardPropagation
 from propagate import feedback_alignment
 
+
 class FeedbackAlignment(BackwardPropagation):
     def set_propagate_functions(self):
         for block in self.sequence:
