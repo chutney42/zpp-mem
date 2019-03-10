6c2d65b68fd5fcf09bb863323ffd2f972baa2f70
Tien Chu <t.chu@student.uw.edu.pl>
2019-03-10 14:39:15 +0100

    Review

--- utils.py
+++ utils.py
@@ -2,5 +2,4 @@ import tensorflow as tf
 
 def sigmoid_prime(x, name=None):
     with tf.name_scope(name or "sigmoid_prime"):
-        res = tf.multiply(tf.sigmoid(x), (tf.constant(1.0) - tf.sigmoid(x)))
-        return res
+        return tf.multiply(tf.sigmoid(x), (tf.constant(1.0) - tf.sigmoid(x)))
