f542fc392c1f5c00a573edf3d4948449a12f8f34
Adam Wiktor <adam.wiktor@vimncontractor.com>
2019-03-17 20:02:51 +0100

    Add possibility to run only chosen tests

--- execute_experiments.sh
+++ execute_experiments.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 
-ret=0
-i=0
+number_regex='^[0-9]+$'
 folder=network_metadata
 mkdir $folder
-while [ $ret -eq 0 ]; do
-    echo "start experiment $i"
-    /usr/bin/time -lp python experiment.py -id $i >$folder/$i.out 2>$folder/$i.err
-    ret=$?
-    ((i++))
-done
-rm $folder/$(($i-1)).*
-for a in $(seq 0 $(($i-2))); do
-    file_name=$(grep -m 1 data $folder/$a.out)
-    if [[ ! -z "$file_name" ]]; then
-        mv $folder/$a.out $folder/_$file_name
-        mv $folder/$a.err $folder/$file_name.err
+
+function run_experiment {
+    echo "run experiment $2"
+    /usr/bin/time -lp python experiment.py $1 $2 >$folder/$3.out 2>$folder/$3.err
+    return $?
+}
+
+function rename_file {
+    file_name=$1
+    new_file_name=$(grep -m 1 data_ $folder/$file_name.out)
+    if [[ ! -z "$new_file_name" ]]; then
+        mv $folder/$file_name.out $folder/_$new_file_name
+        mv $folder/$file_name.err $folder/$new_file_name.err
     fi
+}
+
+i=0
+if [[ ! -z "$1" ]]; then
+    echo "executing chosen experiments"
+    for test_id in $@; do
+        if [[ $test_id =~ $number_regex ]]; then
+            run_experiment -id $test_id $i
+        else
+            run_experiment -name $test_id $i
+        fi
+        ((i++))
+    done
+else
+    echo "executing all experiments"
+    ret=0
+    while [ $ret -eq 0 ]; do
+        run_experiment -id $i $i
+        ret=$i
+        ((i++))
+    done
+    ((i--))
+    rm $folder/$i.*
+fi
+
+for file_name in $(seq 0 $(($i-1))); do
+    rename_file $file_name
 done
 
 cd memory_usage
