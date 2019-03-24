5f33978d32865012c174cb34762fc4a70e9f8645
MironSz <miron.szewczyk@autonomik.pl>
2019-03-24 16:55:30 +0100

    Max pool (#34)
    * Added max_pool layer
    
    * Flatten layer (#32)
    
    * Model to string
    
    * Memory gathering
    
    * Merge files
    
    * Experiments
    
    * Dont append file multiple times, add /usr/bin/time stats
    
    * Add output files to gitignore
    
    * Constructing network from map
    
    * Print network definition
    
    * Print network definition
    
    * Add possibility to run only chosen tests
    
    * Added Flatten layer. Removed unnecessary reshapes
    
    * Fixed issue with dimmensions in DFA
    
    * Removed FlattenLayer. Now FC is responsible for flattening.
    
    * Fixed small issues with BN
    
    * Problem with FullyConnectedManhattan
    
    * Fix merge with master - reduce dimensions of tensor
    
    * Fix experiment executor (#33)
    
    * Fix experiment executor
    
    * Remove usage of /usr/bin/time
    
    * Style
    
    * Added MaxPool and AveragePool layers
    
    * Fixed small typo
    
    * Style

--- definition/blocks_definitions.py
+++ definition/blocks_definitions.py
@@ -1,8 +1,9 @@
 from layer.activation.activation_layer import Sigmoid
 from layer.block import Block
 from layer.util_layer.batch_normalization import BatchNormalization
-from layer.weight_layer.fully_connected import FullyConnected,FullyConnectedManhattan
-from layer.weight_layer.convolutional_layers import ConvolutionalLayer,ConvolutionalLayerManhattan
+from layer.util_layer.max_pool import MaxPool
+from layer.weight_layer.fully_connected import FullyConnected, FullyConnectedManhattan
+from layer.weight_layer.convolutional_layers import ConvolutionalLayer, ConvolutionalLayerManhattan
 
 
 def blocks_50_30_10_bn_sigmoid(output_size):
@@ -17,16 +18,25 @@ def blocks_50_30_10_bn_bm_sigmoid(output_size):
             Block([FullyConnectedManhattan(output_size), Sigmoid()])]
 
 
-def blocks_simple_convoluted(output_size):
-    return [Block([ConvolutionalLayer((3, 3), number_of_filters=5), BatchNormalization(), Sigmoid()]),
+def blocks_simple_convoluted_with_pool(output_size):
+    return [Block(
+        [ConvolutionalLayer((3, 3), number_of_filters=5), MaxPool([4, 4], [2, 2]), BatchNormalization(), Sigmoid()]),
             Block([ConvolutionalLayer((3, 3), number_of_filters=5), BatchNormalization(), Sigmoid()]),
-            Block([FullyConnected(30,flatten=True), BatchNormalization(), Sigmoid()]),
+            Block([FullyConnected(30, flatten=True), BatchNormalization(), Sigmoid()]),
             Block([FullyConnected(output_size), Sigmoid()])]
+
+def blocks_simple_convoluted(output_size):
+    return [Block(
+        [ConvolutionalLayer((3, 3), number_of_filters=5), BatchNormalization(), Sigmoid()]),
+            Block([ConvolutionalLayer((3, 3), number_of_filters=5), BatchNormalization(), Sigmoid()]),
+            Block([FullyConnected(30, flatten=True), BatchNormalization(), Sigmoid()]),
+            Block([FullyConnected(output_size), Sigmoid()])]
+
 
 def blocks_simple_convoluted_bm(output_size):
     return [Block([ConvolutionalLayerManhattan((3, 3), number_of_filters=5), BatchNormalization(), Sigmoid()]),
             Block([ConvolutionalLayerManhattan((3, 3), number_of_filters=5), BatchNormalization(), Sigmoid()]),
-            Block([FullyConnectedManhattan(30,flatten=True), BatchNormalization(), Sigmoid()]),
+            Block([FullyConnectedManhattan(30, flatten=True), BatchNormalization(), Sigmoid()]),
             Block([FullyConnectedManhattan(output_size), Sigmoid()])]
 
 
---
+++ layer/util_layer/max_pool.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+
+from layer.layer import Layer
+
+
+class Pool(Layer):
+    def __init__(self, pooling_function, kernel_size, strides, padding="VALID", data_format='NHWC', scope="pool_layer"):
+        super().__init__(trainable=False, scope=scope)
+        self.kernel_size = [1] + kernel_size + [1]
+        self.strides = [1] + strides + [1]
+        if len(self.kernel_size) > 4:
+            self.kernel_size = kernel_size
+        if len(self.strides) > 4:
+            self.strides = strides
+        self.padding = padding
+        self.data_format = data_format
+        self.pooling_function = pooling_function
+        self.pooling_function_name = None
+
+    def __str__(self):
+        return f"{self.pooling_function_name}({self.kernel_size}, {self.strides}, {self.padding})"
+
+    def build_forward(self, input_vec, remember_input=True, gather_stats=True):
+        if remember_input:
+            self.input_vec = input_vec
+        with tf.variable_scope(self.scope, tf.AUTO_REUSE):
+            output = self.pooling_function(input_vec, self.kernel_size, self.strides, self.padding, self.data_format)
+            return output
+
+    def build_backward(self, error, gather_stats=True):
+        with tf.variable_scope(self.scope):
+            pre_pool = self.restore_input()
+            post_pool = self.pooling_function(pre_pool, self.kernel_size, self.strides, self.padding, self.data_format)
+            backprop_error = tf.gradients(post_pool, pre_pool, error)
+            return backprop_error[0]
+
+
+class MaxPool(Pool):
+    def __init__(self, *args):
+        super().__init__(tf.nn.max_pool, scope="max_pool", *args)
+        self.pooling_function_name = "MaxPool"
+
+
+class AveragePool(Pool):
+    def __init__(self, *args):
+        super().__init__(tf.nn.avg_pool, scope="average_pool", *args)
+        self.pooling_function_name = "AveragePool"
