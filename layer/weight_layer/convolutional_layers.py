75b30870f54c3b6cdfcaa0c878a14d3fbd6e9290
Kamil Mykitiuk <Kamil.Mykitiuk@vimncontractor.com>
2019-03-27 15:10:20 +0100

    put_kernels_on_grid clean up

--- layer/weight_layer/convolutional_layers.py
+++ layer/weight_layer/convolutional_layers.py
@@ -87,8 +87,7 @@ def put_kernels_on_grid(kernel, grid_Y=None, grid_X=None, pad=1):
         from numpy.ma import sqrt
         for i in range(int(sqrt(float(n))), 0, -1):
             if n % i == 0:
-                if i == 1: print('Who would enter a prime number of filters')
-                return (i, int(n / i))
+                return i, int(n / i)
 
     if grid_Y is None:
         (grid_Y, grid_X) = factorization(kernel.get_shape()[3].value)
@@ -96,33 +95,16 @@ def put_kernels_on_grid(kernel, grid_Y=None, grid_X=None, pad=1):
     x_min = tf.reduce_min(kernel)
     x_max = tf.reduce_max(kernel)
 
-    kernel1 = (kernel - x_min) / (x_max - x_min)
+    kernel = (kernel - x_min) / (x_max - x_min)
 
-    # pad X and Y
-    x1 = tf.pad(kernel1, tf.constant([[pad, pad], [pad, pad], [0, 0], [0, 0]]), mode='CONSTANT')
-
-    # X and Y dimensions, w.r.t. padding
-    Y = kernel1.get_shape()[0] + 2 * pad
-    X = kernel1.get_shape()[1] + 2 * pad
-
-    channels = kernel1.get_shape()[2]
-
-    # put NumKernels to the 1st dimension
-    x2 = tf.transpose(x1, (3, 0, 1, 2))
-    # organize grid on Y axis
-    x3 = tf.reshape(x2, tf.stack([grid_X, Y * grid_Y, X, channels]))  # 3
-
-    # switch X and Y axes
-    x4 = tf.transpose(x3, (0, 2, 1, 3))
-    # organize grid on X axis
-    x5 = tf.reshape(x4, tf.stack([1, X * grid_X, Y * grid_Y, channels]))  # 3
-
-    # back to normal order (not combining with the next step for clarity)
-    x6 = tf.transpose(x5, (2, 1, 3, 0))
-
-    # to tf.image_summary order [batch_size, height, width, channels],
-    #   where in this case batch_size == 1
-    x7 = tf.transpose(x6, (2, 0, 1, 3))
-
-    # scale to [0, 255] and convert to uint8
-    return tf.image.convert_image_dtype(x7, dtype=tf.uint8)
+    x = tf.pad(kernel, tf.constant([[pad, pad], [pad, pad], [0, 0], [0, 0]]), mode='CONSTANT')
+    Y = kernel.get_shape()[0] + 2 * pad
+    X = kernel.get_shape()[1] + 2 * pad
+    channels = kernel.get_shape()[2]
+    x = tf.transpose(x, (3, 0, 1, 2))
+    x = tf.reshape(x, tf.stack([grid_X, Y * grid_Y, X, channels]))  # 3
+    x = tf.transpose(x, (0, 2, 1, 3))
+    x = tf.reshape(x, tf.stack([1, X * grid_X, Y * grid_Y, channels]))  # 3
+    x = tf.transpose(x, (2, 1, 3, 0))
+    x = tf.transpose(x, (2, 0, 1, 3))
+    return tf.image.convert_image_dtype(x, dtype=tf.uint8)
