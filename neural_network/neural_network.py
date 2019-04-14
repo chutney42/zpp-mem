fddb1d53ca35cf5ef62beaf6376d600cfdb7d6c6
Kamil Mykitiuk <Kamil.Mykitiuk@vimncontractor.com>
2019-04-14 14:36:29 +0200

    Remove debug lines

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -164,7 +164,6 @@ class NeuralNetwork(object):
         if self.gather_stats or self.memory_only:
             writer = tf.summary.FileWriter(f"./demo/{self.scope}_{self.run_number}", self.sess.graph)
             val_writer = tf.summary.FileWriter(f"./demo/val_{self.scope}_{self.run_number}", self.sess.graph)
-            exit(0)
             return writer, val_writer
         else:
             return None, None
