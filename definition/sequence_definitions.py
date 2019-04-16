560f3f9455accd078633f0ffb33f8cf0878ea67b
Tien Chu <t.chu@student.uw.edu.pl>
2019-04-16 19:25:56 +0200

    YOYO

--- definition/sequence_definitions.py
+++ definition/sequence_definitions.py
@@ -7,78 +7,6 @@ def fc0(output_size):
             FullyConnected(30), Sigmoid(),
             FullyConnected(output_size), Sigmoid()]
 
-
-def fc1(output_size):
-    return [FullyConnected(50, flatten=True), BatchNormalization(), Sigmoid(),
-            FullyConnected(30), BatchNormalization(), Sigmoid(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc2(output_size):
-    return [FullyConnected(100, flatten=True), BatchNormalization(), Sigmoid(),
-            FullyConnected(200), BatchNormalization(), Sigmoid(),
-            FullyConnected(50), BatchNormalization(), Sigmoid(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc3(output_size):
-    return [FullyConnected(200, flatten=True), BatchNormalization(), Sigmoid(),
-            FullyConnected(1000), BatchNormalization(), Sigmoid(),
-            FullyConnected(100), BatchNormalization(), Sigmoid(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc1_relu(output_size):
-    return [FullyConnected(50, flatten=True), BatchNormalization(), ReLu(),
-            FullyConnected(30), BatchNormalization(), ReLu(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc2_relu(output_size):
-    return [FullyConnected(100, flatten=True), BatchNormalization(), ReLu(),
-            FullyConnected(200), BatchNormalization(), ReLu(),
-            FullyConnected(50), BatchNormalization(), ReLu(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc3_relu(output_size):
-    return [FullyConnected(200, flatten=True), BatchNormalization(), ReLu(),
-            FullyConnected(1000), BatchNormalization(), ReLu(),
-            FullyConnected(100), BatchNormalization(), ReLu(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def conv0(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), Sigmoid(),
-            FullyConnected(30, flatten=True), Sigmoid(),
-            FullyConnected(output_size)]
-
-def conv1(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), BatchNormalization(), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), BatchNormalization(), Sigmoid(),
-            FullyConnected(30, flatten=True), BatchNormalization(), Sigmoid(),
-            FullyConnected(output_size)]
-
-
-def conv2(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            FullyConnected(output_size, flatten=True), Sigmoid()]
-
-
-def conv3(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=15, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=15, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=15, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            FullyConnected(output_size, flatten=True), Sigmoid()]
-
-
 def long_fc(output_size):
     sequence = []
     for i in range(30):
@@ -86,14 +14,12 @@ def long_fc(output_size):
     sequence += [FullyConnected(output_size), Sigmoid()]
     return sequence
 
-
 def long_conv(output_size):
     sequence = []
     for i in range(30):
         sequence += [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=5, strides=[1, 1], padding="SAME"), BatchNormalization(), Sigmoid()]
     sequence += [FullyConnected(output_size, flatten=True), Sigmoid()]
     return sequence
-
 
 def resnet_18(output_size):
     return build_resnet(output_size, [2, 2, 2, 2], batch_relu_conv)
@@ -114,98 +40,6 @@ def resnet_101(output_size):
 def resnet_152(output_size):
     return build_resnet(output_size, [3, 8, 36, 3], batch_relu_conv_3)
 
-def fc3(output_size):
-    return [FullyConnected(200, flatten=True), BatchNormalization(), Sigmoid(),
-            FullyConnected(1000), BatchNormalization(), Sigmoid(),
-            FullyConnected(100), BatchNormalization(), Sigmoid(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc1_relu(output_size):
-    return [FullyConnected(50, flatten=True), BatchNormalization(), ReLu(),
-            FullyConnected(30), BatchNormalization(), ReLu(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc2_relu(output_size):
-    return [FullyConnected(100, flatten=True), BatchNormalization(), ReLu(),
-            FullyConnected(200), BatchNormalization(), ReLu(),
-            FullyConnected(50), BatchNormalization(), ReLu(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def fc3_relu(output_size):
-    return [FullyConnected(200, flatten=True), BatchNormalization(), ReLu(),
-            FullyConnected(1000), BatchNormalization(), ReLu(),
-            FullyConnected(100), BatchNormalization(), ReLu(),
-            FullyConnected(output_size), Sigmoid()]
-
-
-def conv0(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), Sigmoid(),
-            FullyConnected(30, flatten=True), Sigmoid(),
-            FullyConnected(output_size)]
-
-def conv1(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), BatchNormalization(), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"), BatchNormalization(), Sigmoid(),
-            FullyConnected(30, flatten=True), BatchNormalization(), Sigmoid(),
-            FullyConnected(output_size)]
-
-
-def conv2(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=10, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            FullyConnected(output_size, flatten=True), Sigmoid()]
-
-
-def conv3(output_size):
-    return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=15, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=15, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=15, strides=[1, 1], padding="SAME"),
-            BatchNormalization(), MaxPool([4, 4], [2, 2]), Sigmoid(),
-            FullyConnected(output_size, flatten=True), Sigmoid()]
-
-
-def long_fc(output_size):
-    sequence = []
-    for i in range(30):
-        sequence += [FullyConnected(500, flatten=(i == 0)), BatchNormalization(), Sigmoid()]
-    sequence += [FullyConnected(output_size), Sigmoid()]
-    return sequence
-
-
-def long_conv(output_size):
-    sequence = []
-    for i in range(30):
-        sequence += [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=5, strides=[1, 1], padding="SAME"), BatchNormalization(), Sigmoid()]
-    sequence += [FullyConnected(output_size, flatten=True), Sigmoid()]
-    return sequence
-
-
-def resnet_18(output_size):
-    return build_resnet(output_size, [2, 2, 2, 2], batch_relu_conv)
-
-
-def resnet_34(output_size):
-    return build_resnet(output_size, [3, 4, 6, 3], batch_relu_conv)
-
-
-def resnet_50(output_size):
-    return build_resnet(output_size, [3, 4, 6, 3], batch_relu_conv_3)
-
-
-def resnet_101(output_size):
-    return build_resnet(output_size, [3, 4, 23, 3], batch_relu_conv_3)
-
-
-def resnet_152(output_size):
-    return build_resnet(output_size, [3, 8, 36, 3], batch_relu_conv_3)
 
 def liao_mnist(output_size):
     return [ConvolutionalLayer(filter_dim=(5, 5), num_of_filters=20, strides=[1, 1], padding="VALID"),
